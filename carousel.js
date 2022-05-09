97b9a76dc0e9ea6b3f10e28674d8a0891ac92984
alevann <55453451+alevann@users.noreply.github.com>
2022-05-09 18:25:46 +0200

    Added swipe support to carousel

--- carousel.js
+++ carousel.js
@@ -34,3 +34,47 @@ function select(index) {
 // Select the first image in the carousel
 select(0)
 
+
+
+let start_x = 0
+let start_y = 0
+
+carousel.addEventListener('touchstart', function (e) {
+  const evt = (typeof e.originalEvent === 'undefined') ? e : e.originalEvent
+  const touch = evt.touches[0] || evt.changedTouches[0]
+
+  start_x = touch.screenX
+  start_y = touch.screenY
+})
+
+carousel.addEventListener('touchend', function (e) {
+  const evt = (typeof e.originalEvent === 'undefined') ? e : e.originalEvent
+  const touch = evt.touches[0] || evt.changedTouches[0]
+
+  const y_diff = start_y - touch.screenY
+  const x_diff = start_x - touch.screenX
+
+  // Ignore vertical swipes
+  if (Math.abs(x_diff) < Math.abs(y_diff)) {
+    return
+  }
+
+  // Smallest distance (in px) a swipe must be to be proccessed
+  const swipe_threshold = 100
+  if (Math.abs(x_diff) < swipe_threshold) {
+    return
+  }
+
+  // Consume the event
+  e.preventDefault()
+
+
+  // Swipe left
+  if (x_diff < 0 && selected > 0) {
+    return select(selected-1)
+  }
+  // Swipe right
+  if (x_diff > 0 && selected < images.length - 1) {
+    return select(selected+1)
+  }
+})
